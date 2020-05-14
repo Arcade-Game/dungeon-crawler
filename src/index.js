c8d05bfe4076fae59f684b111189e5a08e546932
Raposhian <ryan.aposhian@gmail.com>
2020-05-14 14:30:12 -0600

    updated to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { HashRouter as Router} from 'react-router-dom';
 import { LastLocationProvider } from 'react-router-last-location';
 import {Provider} from "react-redux";
 import './index.css';
