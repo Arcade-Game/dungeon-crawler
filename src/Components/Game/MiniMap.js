b597116121a57f81f32158c0ae1b8c4ae5866e5a
jondeh <dehlinjd@gmail.com>
2020-09-15 15:01:45 -0600

    changed px to vh for the rest of the game view, making it responsive to different screen sizes.

--- src/Components/Game/MiniMap.js
+++ src/Components/Game/MiniMap.js
@@ -9,7 +9,7 @@ const MiniMap = ({mmX, mmY}) => {
 
     let mappedGrid = grid.map((e,i,a) => i > 4 && i < a.length-5 ? e.map((f,j, z) => { // Iterates over map grid and renders a mini-map div in the mini-map grid for each tile, which is specific to the tile type.
         return j > 4 && j < z.length-5 ? (
-        f.explored === true ? <div className={i === charY && j === charX ? 'mini-map-char' : `mini-map-${f.hidden ? 'wall' : f.mist ? 'mist' : f.type === 'monster' ? f.type : f.type === 'chest' ? f.type : f.elevation === 3 && f.type !== 'cliff' || f.elevation === 2 && f.type !== 'platform' ? f.type-f.elevation : f.type}`}>{f.pushable === true ? <div className="mini-map-pushable" /> : null}</div> : <div className="mini-map-unexplored"></div>
+        f.explored === true ? <div className={i === charY && j === charX ? 'mini-map-char' : `mini-map-${f.hidden ? 'wall' : f.mist ? 'mist' : f.tileType === 'monster' ? f.tileType : f.tileType === 'chest' ? f.tileType : f.elevation === 3 && f.tileType !== 'cliff' || f.elevation === 2 && f.tileType !== 'platform' ? f.tileType-f.elevation : f.tileType}`}>{f.pushable === true ? <div className="mini-map-pushable" /> : null}</div> : <div className="mini-map-unexplored"></div>
         ) : null
     }) : null)
 
